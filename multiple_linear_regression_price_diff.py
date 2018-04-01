329b7bdb2e24a45e5097b9ef83c97d9eb7a9fd00
sumansourav <dsumansourav@gmail.com>
2018-03-31 18:36:11 -0700

    Added project report files

---
+++ multiple_linear_regression_price_diff.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 15:12:27 2018
+
+@author: Suman Dikshit
+"""
+
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_excel('IA1-PanasonicLCDTV.xlsx')  # Using updated excel with price_diff
+dataset.head()
+#Dropping the categorical variables as we already have created dummy variables for them.
+dataset.drop(['Screen Size', 'Pixel', 'Motion Rate', 'Selling Price', 'Seg Avg Price'], axis = 1, inplace=True)
+
+# Put data into a X matrix and a y vector
+X = dataset.iloc[:,1:-1]
+y = dataset.iloc[:,-1]
+
+# Add an intercept to X
+import statsmodels.formula.api as sm
+X['Intercept'] = pd.DataFrame(np.ones((704,1),dtype='int'))
+
+# Build a OLS regression model
+regressor_ols = sm.OLS(endog=y, exog=X).fit()
+regressor_ols.summary()
